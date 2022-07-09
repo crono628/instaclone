567c83f888a24d665865acfec63716ff29306930
crono628 <84817352+crono628@users.noreply.github.com>
2022-07-08 23:02:46 -0400

    added sticky nav

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,24 +4,31 @@ const Card = ({ post }) => {
   console.log(post.avatar);
 
   return (
-    <div className="max-w-md border-8 border-solid rounded-xl border-indigo-600">
-      <div>
-        <img src={post.avatar} alt="AVATAR" />
-        <div>{post.name}</div>
+    <div className="max-w-md my-5 mx-auto bg-slate-400 border-8 border-solid rounded-xl border-indigo-600">
+      <div className="flex flex-row items-center">
+        <img
+          className="w-12 h-12 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+          src={post.avatar}
+          alt="AVATAR"
+        />
+        <div className="pl-2">{post.name}</div>
       </div>
       <img src={post.img} alt="PHOTO" />
-      <div>{post.caption}</div>
-      <div>
-        <div>{post.likes}</div>
-        {/* <div>{post.comments.length}</div> */}
+      <div className="flex justify-between">
+        <div className="pl-2 flex-1">{post.caption}</div>
+        <div className="px-2">👍 {post.likes}</div>
+        <div className="px-2">💬 {post.comments.length}</div>
       </div>
-      <div>
-        {post.comments.map((item) => (
-          <div>
-            <div>{item.user}</div>
-            <div>{item.comment}</div>
-          </div>
-        ))}
+      <hr />
+      <div className="flex flex-col justify-between pl-2">
+        <div>
+          Comments:
+          {post.comments.map((item) => (
+            <div className="flex flex-row">
+              <div>{item.user}</div>--<div>{item.comment}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Nav = ({ user }) => {
   return (
-    <div className="bg-cyan-600">
+    <div className="bg-cyan-600 sticky top-0 z-50">
       <div className="flex max-w-3xl justify-between mx-auto">
         <div className="text-3xl cursor-pointer flex-1 font-bold pl-2 py-2 border-2 border-solid border-yellow-500">
           Petstagram
