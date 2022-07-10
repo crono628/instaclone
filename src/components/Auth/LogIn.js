f94cfc51fc71f5e23e9cbb07382ba3181dddfe5d
crono628 <84817352+crono628@users.noreply.github.com>
2022-07-10 16:35:03 -0400

    Login wired up

--- src/components/Auth/LogIn.js
+++ src/components/Auth/LogIn.js
@@ -18,24 +18,49 @@ const LogIn = () => {
       await login(emailRef.current.value, passwordRef.current.value);
       navigate('/');
     } catch (error) {
-      setError(error.message);
+      setError('Invalid email or password');
+      setTimeout(() => {
+        setError('');
+      }, 3000);
     }
     setLoading(false);
   };
 
   return (
-    <div className="">
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Email</label>
-        <input ref={emailRef} type="email" />
-        <label>Password</label>
-        <input ref={passwordRef} type="password" />
-        <button type="submit">Log In</button>
-      </form>
-      <p>
-        Don't have an account? <Link to="/signup">Sign up</Link>
-      </p>
+    <div className="h-[100vh]">
+      <div className="flex flex-col py-4 mx-auto mt-12 max-w-sm items-center border rounded-md border-black bg-white">
+        <p className="text-2xl">Log In</p>
+        <p className="h-7 text-red-600">{error}</p>
+        <form className="flex flex-col" onSubmit={handleSubmit}>
+          <label>Email</label>
+          <input
+            className="border border-black rounded-md p-1"
+            ref={emailRef}
+            type="email"
+            required
+          />
+          <label>Password</label>
+          <input
+            className="border border-black rounded-md p-1"
+            ref={passwordRef}
+            type="password"
+            required
+          />
+          <button
+            disabled={loading}
+            className="py-2 my-2 rounded-md bg-blue-600 hover:bg-blue-500 text-white"
+            type="submit"
+          >
+            Log In
+          </button>
+        </form>
+        <p>
+          Don't have an account?{' '}
+          <Link className="underline" to="/signup">
+            Sign up
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
