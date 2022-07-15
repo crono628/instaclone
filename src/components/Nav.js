4647bbac9f1b1c643bbb2f22c2519a80c9edb247
crono628 <84817352+crono628@users.noreply.github.com>
2022-07-15 12:21:10 -0400

    nav css updated for sizing responsiveness

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,10 @@ import React, { useRef, useState } from 'react';
 import { useAuth } from './Auth/AuthContext';
 import SearchIcon from '@mui/icons-material/Search';
 import { IconButton } from '@mui/material';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
 export const Nav = () => {
-  const { logout, currentUser } = useAuth();
+  const { currentUser } = useAuth();
   const [search, setSearch] = useState(false);
   const searchRef = useRef();
 
@@ -13,8 +14,9 @@ export const Nav = () => {
   };
 
   return (
-    <div className="bg-cyan-600 sticky top-0 z-50">
-      <div className="flex items-center max-w-5xl justify-between mx-auto">
+    <div>
+      <div className="bg-cyan-600 sticky top-0 z-50 h-14 flex items-center max-w-5xl justify-between mx-auto">
+        {/* If no user, search toggle feature is unavailable */}
         {!currentUser ? (
           <div className="flex text-2xl sm:text-3xl flex-1 font-bold pl-2 py-2 ">
             InstaClone
@@ -24,16 +26,16 @@ export const Nav = () => {
             <IconButton onClick={handleSearch}>
               <SearchIcon />
             </IconButton>
-            <div className="flex text-2xl sm:text-3xl flex-1 font-bold pl-2 py-2 ">
+            <div className="flex text-xl sm:text-2xl flex-1 font-bold   ">
               InstaClone
             </div>
           </div>
         ) : (
-          <div className="flex items-center pl-2 transition-opacity duration-100">
+          <div className="flex items-center ml-2 transition-opacity duration-100">
             <IconButton onClick={handleSearch}>
               <SearchIcon />
             </IconButton>
-            <div className="flex text-2xl sm:text-3xl flex-1 font-bold pl-2 py-2 ">
+            <div className="flex text-xl sm:text-2xl flex-1 font-bold pl-2 py-2">
               <form onSubmit={(e) => e.preventDefault()}>
                 <input
                   ref={searchRef}
@@ -43,16 +45,11 @@ export const Nav = () => {
             </div>
           </div>
         )}
-        {/* {currentUser ? (
-          <div>
-            <button
-              onClick={() => logout()}
-              className="w-max text-2xl sm:text-3xl cursor-pointer font-bold px-2 py-2 "
-            >
-              Log Out
-            </button>
-          </div>
-        ) : null} */}
+        <div className="mr-2">
+          <IconButton>
+            <AddBoxIcon />
+          </IconButton>
+        </div>
       </div>
     </div>
   );
